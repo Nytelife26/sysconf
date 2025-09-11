da67174a202aca17552a449454a627904bf2d2f0
nytelife26 <xtylerjrx@gmail.com>
2025-09-11 14:00:38 +0100

    feat(mod): enable ssh auth for pam in openssh

--- modules/openssh.nix
+++ modules/openssh.nix
@@ -5,6 +5,7 @@
 }: {
 	options.my.openssh = {
 		enable = lib.mkEnableOption "OpenSSH server configuration.";
+		hookPam = lib.mkEnableOption "using SSH authorization for PAM (and sudo).";
 		keys =
 			lib.mkOption {
 				type = lib.types.listOf lib.types.singleLineStr;
@@ -26,5 +27,11 @@
 
 			users.users.${config.my.user.name}.openssh.authorizedKeys.keys =
 				config.my.openssh.keys;
+
+			security.pam =
+				lib.mkIf config.my.openssh.hookPam {
+					sshAgentAuth.enable = true;
+					services.sudo.sshAgentAuth = true;
+				};
 		};
 }
