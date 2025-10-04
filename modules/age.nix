fd66093b7cd48283d6984e48387549638a1267bc
nytelife26 <xtylerjrx@gmail.com>
2025-10-04 17:00:17 +0100

    refactor: prevent loading all secrets to hosts

--- modules/age.nix
+++ modules/age.nix
@@ -1,17 +1,3 @@
 {
-	config,
-	lib,
-	tools,
-	...
-}: {
-	config.age = {
-		identityPaths = ["${config.users.users.${config.my.user.name}.home}/.ssh/id_age"];
-		secrets =
-			lib.listToAttrs (
-				map (path: {
-						name = tools.pathName path;
-						value.file = path;
-					}) (tools.dirFiles ".age" ../secrets)
-			);
-	};
+	config.age.identityPaths = ["/etc/ssh/ssh_host_ed25519_key"];
 }
