ed732c152a5ae22d60d66a0ce9933f12d9e50c50
nytelife26 <xtylerjrx@gmail.com>
2025-11-06 23:39:18 +0000

    refactor(mod): allow theme selection in style

--- modules/style.nix
+++ modules/style.nix
@@ -3,43 +3,62 @@
 	pkgs,
 	lib,
 	...
-}: {
-	options.my.style = lib.mkEnableOption "theme configuration.";
+}: let
+	cfg = config.my.style;
+	themes = {
+		catppuccin-mocha = {
+			base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+			image = pkgs.nixos-artwork.wallpapers.catppuccin-mocha.src;
+		};
+		catppuccin-mocha-oled = {
+			base16Scheme = {
+				system = "base16";
+				name = "Catppuccin Mocha OLED";
+				author = "https://catppuccin.com";
+				variant = "dark";
+				palette = {
+					base00 = "#000000"; # base
+					base01 = "#010101"; # mantle
+					base02 = "#313244"; # surface0
+					base03 = "#45475a"; # surface1
+					base04 = "#585b70"; # surface2
+					base05 = "#cdd6f4"; # text
+					base06 = "#f5e0dc"; # rosewater
+					base07 = "#b4befe"; # lavender
+					base08 = "#f38ba8"; # red
+					base09 = "#fab387"; # peach
+					base0A = "#f9e2af"; # yellow
+					base0B = "#a6e3a1"; # green
+					base0C = "#94e2d5"; # teal
+					base0D = "#89b4fa"; # blue
+					base0E = "#cba6f7"; # mauve
+					base0F = "#f2cdcd"; # flamingo
+				};
+			};
+			image =
+				pkgs.fetchurl {
+					url = "https://raw.githubusercontent.com/gytis-ivaskevicius/high-quality-nix-content/174f162cf06a02f7090986e19720487c19f39416/wallpapers/nix-glow-black.png";
+					hash = "sha256-3AG1n3BrjR/iJVqiSZbj/ZeAZG+SB1zpGsTmY/SDFMk=";
+				};
+		};
+	};
+in {
+	options.my.style = {
+		enable = lib.mkEnableOption "theme configuration.";
+		theme =
+			lib.mkOption {
+				description = "Name of the theme to use.";
+				type = lib.types.enum ["catppuccin-mocha" "catppuccin-mocha-oled"];
+				default = "catppuccin-mocha-oled";
+			};
+	};
 
 	config =
-		lib.mkIf config.my.style {
+		lib.mkIf cfg.enable {
 			stylix = {
 				autoEnable = true;
 				enable = true;
-				base16Scheme = {
-					system = "base16";
-					name = "Catppuccin Mocha OLED";
-					author = "https://catppuccin.com";
-					variant = "dark";
-					palette = {
-						base00 = "#000000"; # base
-						base01 = "#010101"; # mantle
-						base02 = "#313244"; # surface0
-						base03 = "#45475a"; # surface1
-						base04 = "#585b70"; # surface2
-						base05 = "#cdd6f4"; # text
-						base06 = "#f5e0dc"; # rosewater
-						base07 = "#b4befe"; # lavender
-						base08 = "#f38ba8"; # red
-						base09 = "#fab387"; # peach
-						base0A = "#f9e2af"; # yellow
-						base0B = "#a6e3a1"; # green
-						base0C = "#94e2d5"; # teal
-						base0D = "#89b4fa"; # blue
-						base0E = "#cba6f7"; # mauve
-						base0F = "#f2cdcd"; # flamingo
-					};
-				};
-				image =
-					pkgs.fetchurl {
-						url = "https://raw.githubusercontent.com/gytis-ivaskevicius/high-quality-nix-content/174f162cf06a02f7090986e19720487c19f39416/wallpapers/nix-glow-black.png";
-						hash = "sha256-3AG1n3BrjR/iJVqiSZbj/ZeAZG+SB1zpGsTmY/SDFMk=";
-					};
+				inherit (themes.${cfg.theme}) base16Scheme image;
 				fonts = {
 					sizes.applications = 11;
 					serif = {
