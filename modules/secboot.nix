a82b6e1d615f800fd6d831a5dc8fb64debe85889
lveneris <tyler@kludgecs.com>
2026-01-02 22:57:23 +0000

    feat(mod): enable lanzaboote key management

--- modules/secboot.nix
+++ modules/secboot.nix
@@ -13,10 +13,15 @@
 				lanzaboote = {
 					enable = true;
 					pkiBundle = "/var/lib/sbctl";
+					autoGenerateKeys.enable = true;
+					autoEnrollKeys = {
+						enable = true;
+						autoReboot = true;
+					};
 				};
-				# NOTE: lanzaboote replaces systemd-boot for now (as of 0.4.2), review later
+				# NOTE: lanzaboote replaces systemd-boot for now (as of 1.0.0), review later
 				loader = {
-					systemd-boot.enable = false;
+					systemd-boot.enable = lib.mkForce false;
 					efi.canTouchEfiVariables = true;
 				};
 			};
